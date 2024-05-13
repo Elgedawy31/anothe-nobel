0b18a2beed8c73c7f7038b83fbe6e4cd5d6c7ec5
Omar Mohamed <99252081+OmarMohammedd@users.noreply.github.com>
2024-05-13 15:05:00 +0300

    update

--- src/components/industires/Industirs.jsx
+++ src/components/industires/Industirs.jsx
@@ -5,7 +5,7 @@ import { useTranslation } from "react-i18next";
 function Industirs() {
   const { t, i18n } = useTranslation();
   return (
-    <div className="industrial-section ">
+    <div className="industrial-section">
       <Carousel data-bs-theme="dark">
         <Carousel.Item className="carousel-item">
           <div className="d-flex align-items-center justify-content-center h-100 flex-column container text-center">
@@ -17,8 +17,8 @@ function Industirs() {
 
             <p className="industires-description" dir={i18n.language === "en" ? "ltr" : "rtl"}>
               {i18n.language === "en"
-                ? <p>our webinars helps in understanding any thing about an industrial fan <br /> so as to select the optimum operating point for the required  application.</p>
-                : <p>ندواتنا عبر الإنترنت تساعد في فهم أي شيء عن المروحة <br /> الصناعية، مما يتيح اختيار نقطة التشغيل الأمثل للتطبيق المطلوب.</p>
+                ? <p>our webinars helps in understanding any thing about an industrial fan so as to select the optimum operating point for the required  application.</p>
+                : <p>ندواتنا عبر الإنترنت تساعد في فهم أي شيء عن المروحة الصناعية، مما يتيح اختيار نقطة التشغيل الأمثل للتطبيق المطلوب.</p>
                 }
             </p>
           </div>
@@ -33,8 +33,8 @@ function Industirs() {
 
             <p className="industires-description" dir={i18n.language === "en" ? "ltr" : "rtl"}>
               {i18n.language === "en"
-                ? <p>NOBEL CENTRIFUGAL FANS ARE SUITABLE FOR THE EXHAUST OF CLEAN AIR, LIGHTLY DUSTY. <br /> BACKWARD IMPELLERS, WING AND SCIROCCO PROFILE, fan series NT, etc...</p>
-                : <p>مراوح نوبل الطردية مناسبة لتهوية الهواء النظيف والمواطن ذو <br /> الغبار الخفيف. المروحة الدافعة إلى الخلف، الجناح والملف الجانبي، سلسلة مرواح NT، إلخ...</p> }
+                ? <p>NOBEL CENTRIFUGAL FANS ARE SUITABLE FOR THE EXHAUST OF CLEAN AIR, LIGHTLY DUSTY. BACKWARD IMPELLERS, WING AND SCIROCCO PROFILE, fan series NT, etc...</p>
+                : <p>مراوح نوبل الطردية مناسبة لتهوية الهواء النظيف والمواطن ذو الغبار الخفيف. المروحة الدافعة إلى الخلف، الجناح والملف الجانبي، سلسلة مرواح NT، إلخ...</p> }
             </p>
           </div>
         </Carousel.Item>
