51fd95d5d09b8d7c031311b7911abe01f6c409de
Elgedawy31 <mohamedelgedawy40@gmail.com>
2024-05-14 19:41:30 +0300

    updates

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,25 +1,69 @@
 import "./Hero.scss";
 // @ts-ignore
 import hero from "../../assets/hero.png";
-import { useTranslation } from 'react-i18next';
-
+import { useTranslation } from "react-i18next";
+import { Carousel } from "react-bootstrap";
 
 function Hero() {
   const { t, i18n } = useTranslation();
 
   return (
-    <div className="hero-section " style={{ backgroundImage: `url(${hero})` }} dir={i18n.language === "en" ? "ltr" : "rtl"}>
-      <div className="container">
-        <h1>
-           {i18n.language === "en" && "Welcome To Nobel"}
-           {i18n.language === "ar" && "مرحبا بكم في نوبل"}
-          </h1>
-        <h2>
-          {i18n.language === "en" && "Industries"}
-           {i18n.language === "ar" && "الصناعات"}
-        </h2>
-      </div>
-    </div>
+    <Carousel data-bs-theme="light">
+      <Carousel.Item className="carousel-item">
+        <div
+          className="hero-section "
+          style={{ backgroundImage: `url(${hero})` }}
+          dir={i18n.language === "en" ? "ltr" : "rtl"}
+        >
+          <div className="container">
+            <h1>
+              {i18n.language === "en" && "Welcome To Nobel"}
+              {i18n.language === "ar" && "مرحبا بكم في نوبل"}
+            </h1>
+            <h2>
+              {i18n.language === "en" && "Industries"}
+              {i18n.language === "ar" && "الصناعات"}
+            </h2>
+          </div>
+        </div>
+      </Carousel.Item>
+      <Carousel.Item className="carousel-item">
+        <div
+          className="hero-section "
+          style={{ backgroundImage: `url(${hero})` }}
+          dir={i18n.language === "en" ? "ltr" : "rtl"}
+        >
+          <div className="container">
+            <h1>
+              {i18n.language === "en" && "Welcome To Nobel"}
+              {i18n.language === "ar" && "مرحبا بكم في نوبل"}
+            </h1>
+            <h2>
+              {i18n.language === "en" && "Industries"}
+              {i18n.language === "ar" && "الصناعات"}
+            </h2>
+          </div>
+        </div>
+      </Carousel.Item>
+      <Carousel.Item className="carousel-item">
+        <div
+          className="hero-section "
+          style={{ backgroundImage: `url(${hero})` }}
+          dir={i18n.language === "en" ? "ltr" : "rtl"}
+        >
+          <div className="container">
+            <h1>
+              {i18n.language === "en" && "Welcome To Nobel"}
+              {i18n.language === "ar" && "مرحبا بكم في نوبل"}
+            </h1>
+            <h2>
+              {i18n.language === "en" && "Industries"}
+              {i18n.language === "ar" && "الصناعات"}
+            </h2>
+          </div>
+        </div>
+      </Carousel.Item>
+    </Carousel>
   );
 }
 
